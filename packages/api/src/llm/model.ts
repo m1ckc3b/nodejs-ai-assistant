28d7097b7254e830ddac8c548afa225cfc82004b
mickceb <cebulski.mi@gmail.com>
2024-04-16 17:10:11 +0200

    init model

---
+++ packages/api/src/llm/model.ts
@@ -0,0 +1,12 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+
+export const model = new ChatOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  modelName: "gpt-3.5-turbo",
+  // temperature: 1,
+  // topP: 1,
+  // maxTokens: 1000,
+  // frequencyPenalty: 0,
+  // presencePenalty: 0,
+  // stop: []
+})
