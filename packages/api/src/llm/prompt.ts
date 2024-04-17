f79395d47b3f6fbc4523579b64166ae500339ad1
mickceb <cebulski.mi@gmail.com>
2024-04-17 13:59:00 +0200

    improving result

---
+++ packages/api/src/llm/prompt.ts
@@ -0,0 +1,7 @@
+import { ChatPromptTemplate } from "@langchain/core/prompts"; 
+
+export const prompt = ChatPromptTemplate.fromTemplate(`
+    Répond à la question suivante uniquement d'après le context donné: {context}
+    Donne des exemples de code quand cela est nécessaire.
+    Question : {question}`
+)
