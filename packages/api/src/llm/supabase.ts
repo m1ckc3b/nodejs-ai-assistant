1f2a78174549196eb1c6a40c0365d273abf1d292
mickceb <cebulski.mi@gmail.com>
2024-04-17 10:43:15 +0200

    adding comments and fixing errors

--- packages/api/src/llm/supabase.ts
+++ packages/api/src/llm/supabase.ts
@@ -1,74 +1,64 @@
-import { SupabaseVectorStore } from 'langchain/vectorstores/supabase'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { createClient } from '@supabase/supabase-js'
-import type { DocumentToVectorStore } from '../types/documents'
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { createClient } from "@supabase/supabase-js";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import type { Document } from "langchain/document";
 
-const supabaseKey: (string|undefined) = process.env.SUPABASE_API_KEY
-if (!supabaseKey) throw new Error(`Expected SUPABASE_API_KEY`)
+if (!process.env.SUPABASE_API_KEY) throw new Error(`Expected SUPABASE_API_KEY`);
+const supabaseKey = process.env.SUPABASE_API_KEY;
 
-const url: (string|undefined) = process.env.SUPABASE_URL
-if (!url) throw new Error(`Expected env var SUPABASE_URL`)
+if (!process.env.SUPABASE_URL) throw new Error(`Expected env var SUPABASE_URL`);
+const url = process.env.SUPABASE_URL;
 
 /**
  * Create the Vector Store
  *
  * @export
- * @return {*} 
+ * @return vectorStore - The instance of the vector store created
  */
 export async function supabaseVectorStore() {
-  const client = createClient(url, supabaseKey)
+  const client = createClient(url, supabaseKey);
 
-  const vectorStore = new SupabaseVectorStore(
-    new OpenAIEmbeddings(),
-    {
-      client,
-      tableName: 'documents',
-      queryName: 'match_documents',
-    }
-  )
+  const vectorStore = new SupabaseVectorStore(new OpenAIEmbeddings(), {
+    client,
+    tableName: "documents",
+    queryName: "match_documents",
+  });
 
-  return vectorStore
+  return vectorStore;
 }
 
 /**
  * Adding docs to a Supabase Vector store from a PDF file
  *
  * @export
- * @param {} file - The file to add to the vector store
+ * @param file - The file to add to the vector store
  */
 export async function createAndStoreEmbeddings(file: File) {
   try {
-    const docs = await loadingAndSplittingDocuments(file)
-    const vectorStore = await supabaseVectorStore()
-    vectorStore.addDocuments(docs)
-    return docs
-    
-    
-  } catch (e) {
-    console.error("Error: ", e.message)
+    // Get chunks
+    const docs = await loadingAndSplittingDocuments(file);
+    // Init vector store
+    const vectorStore = await supabaseVectorStore();
+    // Adding chunks to vector store
+    vectorStore.addDocuments(docs);
+  } catch (error: any | unknown) {
+    console.error("ERROR: ", error.message);
   }
 }
 
 /**
  * Loading and splitting data from a PDF file
  *
- * @export
- * @param {*} file
- * @return {Promise<DocumentToVectorStore[]>} - Retruns chunks of the PDF file
+ * @param file - The file to load and split
+ * @return Returns chunks of the PDF file
  */
-export async function loadingAndSplittingDocuments(file: File): Promise<DocumentToVectorStore[]> {
-  try {
-    // Loading
-  const loader = new PDFLoader(file)
-  const docs = await loader.load()
+async function loadingAndSplittingDocuments(file: File): Promise<Document[]> {
+  // Loading
+  const loader = new PDFLoader(file);
+  const docs = await loader.load();
   // Splitting
-  const textSplitters = new RecursiveCharacterTextSplitter()
-  const splits = await textSplitters.splitDocuments(docs)
-
-  return splits
-  } catch (error) {
-    return error.message
-  }
+  const textSplitters = new RecursiveCharacterTextSplitter();
+  return await textSplitters.splitDocuments(docs);
 }
