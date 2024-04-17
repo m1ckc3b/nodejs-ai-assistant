5710d98113a9126e5efb3464f0f0942a2e3253bf
mickceb <cebulski.mi@gmail.com>
2024-04-17 15:26:15 +0200

    improving #2 -> give formatting instructions

--- packages/api/src/llm/prompt.ts
+++ packages/api/src/llm/prompt.ts
@@ -2,6 +2,6 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 export const prompt = ChatPromptTemplate.fromTemplate(`
     Répond à la question suivante uniquement d'après le context donné: {context}
-    Donne des exemples de code quand cela est nécessaire.
+    Donne des exemples de code quand cela est nécessaire. Si tu donne des exemples, sépare les de la réponse de 2 sauts à la ligne.
     Question : {question}`
 )
